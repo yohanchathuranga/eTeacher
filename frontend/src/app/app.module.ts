890ed7a2b3d491637820dd50e8484d2c2ca29b6f
yohanchathuranga <yohanchathuranga20@gmail.com>
2020-06-03 17:30:03 +0530

    add

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -16,6 +16,9 @@ import { SchedulerComponent } from './components/scheduler/scheduler.component';
 import { ComponentsModule } from './components/components.module';
 import { ExamplesModule } from './examples/examples.module';
  import { TestingPageComponent } from './Pages/testing-page/testing-page.component';
+ // import { ExamplesModule } from './examples/examples.module';
+import { TestingpagetwoComponent } from './testingpagetwo/testingpagetwo.component';
+import { TestingpagethreeComponent } from './testingpagethree/testingpagethree.component';
 
 
 @NgModule({
@@ -25,7 +28,9 @@ import { ExamplesModule } from './examples/examples.module';
     NavbarComponent,
     FooterComponent,
     TestingPageComponent,
-     SchedulerComponent
+     SchedulerComponent,
+     TestingpagetwoComponent,
+    TestingpagethreeComponent
     
   ],
   imports: [
