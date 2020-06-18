1fffbe90876c128c5b6876f237e382a5fb056eed
yohanchathuranga <47962597+yohanchathuranga@users.noreply.github.com>
2020-06-18 22:58:40 +0530

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -30,6 +30,33 @@ import { BookingService } from './services/booking.service';
 
 
 // import { ComponentsModule } from './components/components.module';
+import { TestingPageComponent } from './Pages/testing-page/testing-page.component';
+import { TestingpagetwoComponent } from '../app/Pages/testingpagetwo/testingpagetwo.component';
+import { TestingpagethreeComponent } from '../app/Pages/testingpagethree/testingpagethree.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { CreateThreadComponent } from './Pages/testingpagetwo/create-thread/create-thread.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {MatTabsModule} from '@angular/material/tabs';
+//import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+import {NgxPaginationModule} from 'ngx-pagination';
+import {MatDialogModule} from '@angular/material/dialog';
+import { ViewThreadComponent } from './Pages/testingpagetwo/view-thread/view-thread.component';
+import { AngularEditorModule } from '@kolkov/angular-editor';
+import { ReplyCommentComponent } from './Pages/testingpagetwo/view-thread/reply-comment/reply-comment.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSelectModule} from '@angular/material/select';
+import { ToastrModule } from 'ngx-toastr';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { ForumTypeComponent } from './Pages/testingpagetwo/forum-type/forum-type.component';
+
+
 
 
 
@@ -48,7 +75,14 @@ import { BookingService } from './services/booking.service';
     UserbookingsComponent,
     TeacherbookingsComponent,
     BookingdetailteacherComponent,
-    BookingdetailuserComponent
+    BookingdetailuserComponent,
+    TestingPageComponent,
+    TestingpagetwoComponent,
+    TestingpagethreeComponent,
+    CreateThreadComponent,
+    ViewThreadComponent,
+    ReplyCommentComponent,
+    ForumTypeComponent,
     
   ],
   imports: [
@@ -62,6 +96,32 @@ import { BookingService } from './services/booking.service';
     HttpModule,
     HttpClientModule,
     NgxPaginationModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatTabsModule,
+   // MatPaginatorModule,
+    MatSortModule,
+    NgxPaginationModule,
+    MatDialogModule,
+    FullCalendarModule,
+    AngularEditorModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    ToastrModule.forRoot(),
+    MatSnackBarModule
+   
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  entryComponents: [
+    CreateThreadComponent
+  ]
   
     
   ],
