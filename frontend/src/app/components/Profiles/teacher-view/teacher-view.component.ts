204ff5ddc123ce3cc993c5ed3ad224e32ed2015b
Yohan Chathuranga <yohanchathuranga20@gmail.com>
2020-07-24 19:51:56 +0530

    add profiles

---
+++ frontend/src/app/components/Profiles/teacher-view/teacher-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-view',
+  templateUrl: './teacher-view.component.html',
+  styleUrls: ['./teacher-view.component.css']
+})
+export class TeacherViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
