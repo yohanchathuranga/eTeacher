ae54547f3c53dab8119c6e90132cbf2819693a26
yohanchathuranga <47962597+yohanchathuranga@users.noreply.github.com>
2020-07-31 19:35:29 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const router = express.Router()
 const cors = require('cors')
+
 const bodyParser=require('body-parser');
 var forumController = require('./controller/forumController');
 var cmtThreadController = require('./controller/cmtThreadController');
